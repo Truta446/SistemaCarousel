e34ee706c7a82d9023f689babe84fa6dd8fcf87a
V1CT4R4! <juan.versolato@outlook.com>
2019-08-31 18:29:51 -0400

    Buscando dados no bd

--- controllers/buscaController.php
+++ controllers/buscaController.php
@@ -1,21 +1,17 @@
 <?php
     require_once("conexao.php");
     
-    $sql = "SELECT * FROM album_fotos WHERE id_foto = 1";
+    $sql = "SELECT * FROM album_fotos";
 
     $stmt = $mysqli->query($sql);
     $data = array();
 
     while($row = $stmt->fetch_assoc()){
         foreach($row as $key => $val){
-            if (is_null($val))
-                $val = '';
-                $d["$key"] = $val;
-            }
+            $val = is_null($val) ? '' : $val;
+            $d["$key"] = $val;
+        }
         array_push($data, $d);	
     }
 
     $stmt->close();
-
-    echo json_encode($data);
-
